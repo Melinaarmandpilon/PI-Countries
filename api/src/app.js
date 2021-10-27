915eea7a91316d3871bc5ca5c6f28563496ea507
Melina Armand Pilón <meapilon@gmail.com>
2021-10-27 16:59:06 -0300

    Middleware de cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors=require("cors")
 
 require('./db.js'); //base de datos
 
@@ -9,6 +10,7 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
@@ -20,6 +22,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');//metodos que voy a aceptar
   next();
 });
+
 
 server.use('/', routes);
 
