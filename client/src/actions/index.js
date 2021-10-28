0b57821f2a849088c0bf5d586c7cc83e632d380d
Melina Armand Pilón <meapilon@gmail.com>
2021-10-28 19:16:12 -0300

    actionSearch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,30 @@
-import { GET_COUNTRIES, SEARCH } from "./types";
-import axios from "axios"
+import { GET_COUNTRIES, SEARCH_BY_NAME } from "./types";
+import axios from "axios";
 
-
-export function getCountries(){
-    return async function (dispatch){
-        try {
-         const resDbCountries= await axios.get("http://localhost:3001/countries")
-         const data=resDbCountries.data;
-         console.log("QUE ME TRAE MI BASE DE DATOS", data)
-         dispatch({type:GET_COUNTRIES,payload:data})
-        } catch (error) {
-            console.log("error",error)
-        }
+export function getCountries() {
+  return async function (dispatch) {
+    try {
+      const resDbCountries = await axios.get("http://localhost:3001/countries");
+      const data = resDbCountries.data;
+      console.log("QUE ME TRAE MI BASE DE DATOS", data);
+      dispatch({ type: GET_COUNTRIES, payload: data });
+    } catch (error) {
+      console.log("error", error);
     }
+  };
 }
 
-export function search(){
-    return{
-        type:SEARCH
-    }
+export function searchByName(search) {
+  return function (dispatch) {
+    axios("http://localhost:3001/countries?name=" + search)
+      .then((info) => {
+        dispatch({
+          type: SEARCH_BY_NAME,
+          payload: info.data,
+        });
+      })
+      .catch((error) => {
+        console.log("Este error se debe a: " + error);
+      });
+  };
 }
