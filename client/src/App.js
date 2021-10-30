4c981e41bc1b6fd1016119b64ccf1f4b92c402af
Melina Armand Pilón <meapilon@gmail.com>
2021-10-29 23:43:30 -0300

    Se agregaron rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {Route} from 'react-router-dom';
 import {Landing,Home} from './Views/index.js';
+import {CountryDetail} from './components/index';
 
 
 export default function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Landing}/>
-      <Route path="/Home" component={Home}/>
+      <Route exact path="/Home" component={Home}/>
+      <Route exact path="/Home/:id" component={CountryDetail}/>
     </div>
   );
 }
