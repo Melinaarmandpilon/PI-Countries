37670d5123f36c50fb9de223061fe6896f355dc2
Melina Armand Pilón <meapilon@gmail.com>
2021-10-31 02:59:28 -0300

    probando coninput

--- client/src/components/Filter/Continent.jsx
+++ client/src/components/Filter/Continent.jsx
@@ -2,37 +2,50 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { filterByContinent } from "../../actions";
 
+
 export default function Continent() {
   const dispatch = useDispatch();
   const filterCountries = useSelector((state) => state.filterCountries);
-
+ 
   const handleChange = (evento) => {
     console.log("evento.target.value", evento.target);
     dispatch(filterByContinent(evento.target.value));
   };
 
-  let continent=filterCountries?.map(cont=>cont.continent)//me retorna un array con todos los continentes por c/u de los paises=250el
-//   let uniqueContinent =[... new Set(continent)]
-  let uniqueContinent =[]
-  continent.forEach(c=>{
-      if(!uniqueContinent.includes(c)){
-        uniqueContinent.push(c);
-      }
-  })
+  let continent = filterCountries?.map((cont) => cont.continent); //me retorna un array con todos los continentes por c/u de los paises=250el
+  let uniqueContinent = [...new Set(continent)];
 
-  console.log("uniqueContinent",uniqueContinent)
+  // let uniqueContinent =[]
+  // continent.forEach(c=>{
+  //     if(!uniqueContinent.includes(c)){
+  //       uniqueContinent.push(c);
+  //     }
+  // })
+
+  console.log("uniqueContinent", uniqueContinent);
+  
 
   return (
-    <div>
+    <div onChange={handleChange}>
       <label>Filter by Continent</label>
-      <select onChange={handleChange}>
-        <option value="All">All</option>
-        {uniqueContinent?.map((el) => (
-          <option value={el}> {el}</option>
-        ))}
-      </select>
+      {/* <select onChange={handleChange}> */}
+      <div>
+
+       <label> <input type="checkbox" value="All" /* checked={!true} */ />All</label>
+      </div>
+        {/* <option value="All">All</option> */}
+        {uniqueContinent?.map((el,index) => (
+          // <option value={el} key={index}> {el}</option>
+          <label key={index}>
+        <input type="checkbox" value={el}  /* checked={false} */ />
+        {el}
+        </label>
+          ))}
+      {/* </select> */}
     </div>
   );
 }
 
-//   <input type="checkbox" />
+// {/* <div>
+//   <input type="reset" value="">
+// </div>; */}
