34977675887c7226811bfb97d59ffff0f7086c9e
Melina Armand Pilón <meapilon@gmail.com>
2021-11-09 01:36:09 -0300

    cambio de browserRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+import styles from './App.module.css';
 import {Route} from 'react-router-dom';
 import {Landing,Home} from './Views/index.js';
 import {CountryDetail,AddActivity} from './components/index';
-
-
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <BrowserRouter>
       <Route exact path="/" component={Landing}/>
       <Route exact path="/Home" component={Home}/>
       <Route exact path="/Home/:id" component={CountryDetail}/>
       <Route exact path="/activity" component={AddActivity}/>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import store from './store/index';
-import { BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/index";
+
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
